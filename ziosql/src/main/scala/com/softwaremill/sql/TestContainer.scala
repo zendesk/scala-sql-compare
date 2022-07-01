37bc9364e8d2068a72b055581fdf11c03ffd7e35
Lachlan Deck <lachlan.deck@zendesk.com>
2022-07-01 11:34:23 +1000

    fix ZIO / ZManaged deprecations

--- ziosql/src/main/scala/com/softwaremill/sql/TestContainer.scala
+++ ziosql/src/main/scala/com/softwaremill/sql/TestContainer.scala
@@ -9,16 +9,16 @@ import org.flywaydb.core.Flyway
 object TestContainer {
 
   def container[C <: SingleContainer[_]: Tag](c: C): ZLayer[Any, Throwable, C] =
-    ZManaged.make {
-      ZIO.effectBlocking {
+    ZManaged.acquireReleaseWith {
+      ZIO.attemptBlocking {
         c.start()
         c
       }
-    }(container => ZIO.effectBlocking(container.stop()).orDie).toLayer
+    }(container => ZIO.attemptBlocking(container.stop()).orDie).toLayer
 
   def postgres(imageName: String = "postgres:alpine"): ZManaged[Any, Throwable, PostgreSQLContainer] =
-    ZManaged.make {
-      ZIO.effectBlocking {
+    ZManaged.acquireReleaseWith {
+      ZIO.attemptBlocking {
         val c = new PostgreSQLContainer(
           dockerImageNameOverride = Option(imageName).map(DockerImageName.parse)
         )
@@ -28,6 +28,6 @@ object TestContainer {
         c
       }
     } { container =>
-      ZIO.effectBlocking(container.stop()).orDie
+      ZIO.attemptBlocking(container.stop()).orDie
     }
 }
